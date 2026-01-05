5be795df7e2daf11d66d4f3ad1f2a02652eb15aa
swarnaashop <sarnaaphrodite@gmail.com>
2026-01-05 17:27:11 +0600

    Employee List

---
+++ src/Jan5/Employee.java
@@ -0,0 +1,23 @@
+
+package Jan5;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Employee {
+    public static void main(String[] args){
+    
+       Scanner sc = new Scanner(System.in);
+       ArrayList<Object> list = new ArrayList<>();
+       System.out.print("Enter Id: ");
+       int id = sc.nextInt();
+       sc.nextLine();
+       System.out.print("Enter Name: ");
+       double salary = sc.nextDouble();
+       
+       list.add(id);
+       list.add(name);
+       list.add(salary);
+       
+    }
+    
+}
