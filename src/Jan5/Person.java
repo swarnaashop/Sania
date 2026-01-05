b615ed5019ca61cd51e7166ebc1f776bf54d1c4e
swarnaashop <sarnaaphrodite@gmail.com>
2026-01-05 18:07:51 +0600

    Person Final

---
+++ src/Jan5/Person.java
@@ -0,0 +1,34 @@
+
+package Jan5;
+import java.util.ArrayList;
+import java.util.List;
+        
+ public class Person {
+      int id ;
+     String name;
+     double salary;
+     
+     public Person(int id, String name, double salary) {
+         this.id=id;
+         this.name=name;
+         this.salary=salary;
+     }
+     void display(){
+         System.out.println("ID:" + id +",Name: " 
+              + name + ",Salary: " + salary);
+     }
+     public static void main(String[] args) {
+         List<Person> persons = new ArrayList<>();
+         persons.add(new Person(1, "Rahim", 25000));
+         persons.add(new Person(1, "Karim", 30000));
+         persons.add(new Person(1, "Labib", 40000));
+         for (Person p : persons) {
+             p. display();
+         }
+     }
+     
+         
+                 
+     }
+     
+ 
